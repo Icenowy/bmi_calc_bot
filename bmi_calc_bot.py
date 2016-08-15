2199a42f619361c75b62ce4600d494233d053abb
Icenowy Zheng <icenowy@aosc.xyz>
2016-08-15 14:51:14 +0800

    initial implementation
    Signed-off-by: Icenowy Zheng <icenowy@aosc.xyz>

--- bmi_calc_bot.py
+++ bmi_calc_bot.py
@@ -1,3 +1,38 @@
 #! /usr/bin/env python3
 import config
-print(config.a)
+
+import telegram
+
+from telegram.ext import Updater, CommandHandler
+updater = Updater(token=config.token)
+dispatcher = updater.dispatcher
+
+def bmi(bot, update):
+    text = update.message.text
+    texts = text.split(' ')
+    try:
+        height = float(texts[1])
+        weight = float(texts[2])
+        if (height > 3):
+            height /= 100.0
+        outtext = str(weight/height/height)
+    except ValueError:
+        outtext = 'invalid input, format is: /bmi height(in cm or m) weight(in kg)'
+    bot.sendMessage(update.message.chat_id, text=outtext)
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+dispatcher.add_handler(CommandHandler("bmi", bmi))
+
+updater.start_polling()
+
+dispatcher.add_error_handler(error)
+
+updater.idle()
